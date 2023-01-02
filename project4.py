1bfeff30f924cff463263992515f9e9f161836cc
safiraxx <119837148+safiraxx@users.noreply.github.com>
2023-01-02 14:30:42 +0700

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,13 @@
+def bendungan(nama, fungsi, lokasi, ukuran):
+    nama = print("Nama Bendungan : ", nama)
+    fungsi = print("Fungsi Bendungan : ", fungsi)
+    nama = print("Lokasi Bendungan : ", lokasi)
+    ukuran = print("Ukuran Bendungan : ", ukuran)
+    return nama, fungsi, lokasi, ukuran
+
+nama = input("Nama Bendungan : ")
+fungsi = input("Fungsi Bendungan : ")
+lokasi = input("Lokasi Bendungan : ")
+ukuran = input("Ukuran : ")
+
+bendungan(nama, fungsi, lokasi, ukuran)
